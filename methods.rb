079c9f9d17e512a190009a9ccdbc7643e5edb36a
Christopher Nance <cdnance1829@gmail.com>
2022-07-15 23:38:12 -0400

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+#greet_programmer()
+
+def greet_name name 
+    puts "Hello, #{name}!"
+end
+
+#greet_name('Chris')
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!" 
+end
+
+#greet_with_default()
+
+def add num1, num2
+    num1 + num2
+end
+
+#p add(3,5)
+
+def halve number
+    if(number.class != Integer)
+        return nil
+    end
+    number/2
+end
+
+#pp halve(8)
+
